f82f666bb3785b320d2c506823436e5295b0387c
AllenZoo <allenzhu2000@hotmail.com>
2024-01-05 12:23:11 -0800

    fixed multiple loot bag destruction bug

--- Assets/Scripts/Components/Interactables/InteractableLootBag.cs
+++ Assets/Scripts/Components/Interactables/InteractableLootBag.cs
@@ -20,12 +20,15 @@ public class InteractableLootBag : MonoBehaviour, IInteractable
     [Tooltip("Reference to the loot holder that contains information about the loot bag.")]
     [SerializeField] private LootHolder lootHolder;
 
+    [SerializeField] private GameObject parentObject;
+
     // [Tooltip("The inventory shared between loot bags, which is displayed on the UI.")]
     private SO_Inventory lootBagDisplayInventory;
 
     private void Awake()
     {
         Assert.IsNotNull(lootHolder);
+        Assert.IsNotNull(parentObject);
     }
 
     private void Start()
@@ -63,14 +66,36 @@ public class InteractableLootBag : MonoBehaviour, IInteractable
     // Check if inventory is empty. If so, destroy the loot bag.
     private void LootBagDisplayInventory_OnInventoryDataChange()
     {
-        // Debug.Log("Checking whether to destroy loot bag.");
+        
         if (lootBagDisplayInventory.IsEmpty())
         {
-            Debug.Log("Loot bag empty. Destroy loot bag! " + gameObject.transform.parent.name);
-            // Destroy(gameObject.transform.parent.gameObject);
+            if (parentObject.activeSelf == false)
+            {
+                return;
+            }
+
+            StartCoroutine(DeactivateLootbag());
+
+            // This works for preventing bug. 
+            // transform.parent.gameObject.transform.position = new Vector3(0, -100, 0);
         }
     }
 
-    
+    // Known bug this coroutine fixes.
+    // BUG: bug where when player is touching more than 1 loot bag, emptying out one, destroys the other 
+    // as well. Could be something to do with the SO being empty during the process of emptying out the loot bag?
+    // And since the event gets called twice, it destroys the other loot bag as well.
+    private IEnumerator DeactivateLootbag()
+    {
+        yield return new WaitForSeconds(0.2f);
+        if (lootBagDisplayInventory.IsEmpty())
+        {
+            if (parentObject.activeSelf != false)
+            {
+                Debug.Log("Loot bag empty. Destroy loot bag! " + gameObject.transform.parent.name);
+                parentObject.SetActive(false);
+            }
+        }
+    }
 
 }
