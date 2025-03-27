af9dd6fd76bcdca2562d76d8a34c2f6a4adfc37d
AllenZoo <allenzhu2000@hotmail.com>
2025-03-26 21:41:21 -0700

    [UB-124] cleaned up bar controller code

--- Assets/Scripts/General UI/BarController/BossBattleHealthBarSpawner.cs
+++ Assets/Scripts/General UI/BarController/BossBattleHealthBarSpawner.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 // Class that spawns (or sets active) a boss health bar when the boss is aggroed to player.
-// TODO: REFACTOR
 public class BossBattleHealthBarSpawner : MonoBehaviour
 {
     [SerializeField, Required] private LocalEventHandler localEventHandler;
@@ -16,15 +15,7 @@ public class BossBattleHealthBarSpawner : MonoBehaviour
         Assert.IsNotNull(localEventHandler);
         Assert.IsNotNull(barContext);
 
-        if (localEventHandler == null)
-        {
-            localEventHandler = GetComponentInParent<LocalEventHandler>();
-            if (localEventHandler == null)
-            {
-                Debug.LogError("LocalEventHandler unassigned and not found in parent for object [" + gameObject +
-                                        "] with root object [" + gameObject.transform.root.name + "] for BossBattleHealthBarSpawner.cs");
-            }
-        }
+        localEventHandler = InitializerUtil.FindComponentInParent<LocalEventHandler>(this.gameObject);
     }
 
     private void Start()
