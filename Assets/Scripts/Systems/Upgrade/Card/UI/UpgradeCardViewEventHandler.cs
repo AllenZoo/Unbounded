30a9d104e5820d31549a1a8bc572441563f07a5d
AllenZoo <allenzhu2000@hotmail.com>
2025-04-21 11:50:36 -0700

    [UB-143] added doc

--- Assets/Scripts/Systems/Upgrade/Card/UI/UpgradeCardViewEventHandler.cs
+++ Assets/Scripts/Systems/Upgrade/Card/UI/UpgradeCardViewEventHandler.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 
 /// <summary>
 /// Script to handle OnClick, hover events, etc. on the Upgrade Card.
+/// Should only handle logic of passing Upgrade Card to any listeners for these events.
+/// Hover animations should be hanlded by MenuEventSystemHandler.cs
 /// </summary>
 [RequireComponent(typeof(UpgradeCardView))]
 public class UpgradeCardViewEventHandler : MonoBehaviour,
