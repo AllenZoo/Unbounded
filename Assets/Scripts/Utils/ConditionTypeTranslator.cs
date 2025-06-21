dc9b3be4505fbf5a98e385b63474067e6188c931
AllenZoo <allenzhu2000@hotmail.com>
2025-06-20 17:59:37 -0700

    [UB-151] fixed singleton bugs
    - made it so that no singletons have DontDestroyOnLoad.
    - fixed UIOverlayManager Instance being annihilated for some reason

--- Assets/Scripts/Utils/ConditionTypeTranslator.cs
+++ Assets/Scripts/Utils/ConditionTypeTranslator.cs
@@ -3,24 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Utility class for mapping an enum condition type to a concrete condition.
-public class ConditionTypeTranslator : MonoBehaviour
+public class ConditionTypeTranslator : Singleton<ConditionTypeTranslator>
 {
-    public static ConditionTypeTranslator Instance;
-
     private Dictionary<ConditionType, IItemCondition> conditionTypeToCondition;
 
     private void Awake()
     {
-        // Check only one instance of this class exists
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(this.transform.parent.gameObject);
-        }
+        base.Awake();
 
         Init();
     }
