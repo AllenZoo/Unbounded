0ae3ac06314965e29efd36d5620b337c42d492f1
AllenZoo <allenzhu2000@hotmail.com>
2025-06-25 12:17:22 -0700

    [UB-151] added sick dissolve shader

---
+++ Assets/Scripts/Systems/Objective/Highlightable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Component class to be attached to objects that can generate a highlight effect.
+/// </summary>
+public class Highlightable : MonoBehaviour
+{
+    
+    public void Highlight()
+    {
+
+    }
+}
