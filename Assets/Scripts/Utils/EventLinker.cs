1c2572cb80fb122652ea5e6c894d8002eb1aa328
AllenZoo <allenzhu2000@hotmail.com>
2025-06-30 19:43:11 -0700

    [UB-151] implemented event linker

--- Assets/Scripts/Utils/EventLinker.cs
+++ Assets/Scripts/Utils/EventLinker.cs
@@ -1,20 +1,68 @@
+using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 /// <summary>
 /// Used to link events to function calls via inspector.
 /// </summary>
-public class EventLinker : MonoBehaviour
+public class EventLinker : SerializedMonoBehaviour
 {
-    public Action trigger;
-    public UnityEvent e;
-    //public delegate
+    [Tooltip("Hacky way to get type of event. Don't need to fill in the instance.")]
+    [Required] public IGlobalEvent globalEventType;
+    public UnityEvent OnEventTriggered;
+
+    private object eventBinding;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(globalEventType);
+    }
+
+    private void Start()
+    {
+        Type eventType = globalEventType.GetType();
+
+        Type bindingType = typeof(EventBinding<>).MakeGenericType(eventType);
+        eventBinding = Activator.CreateInstance(bindingType, (Action)Trigger);
+
+        Type busType = typeof(EventBus<>).MakeGenericType(eventType);
+        var registerMethod = busType.GetMethod("Register");
+        registerMethod.Invoke(null, new object[] { eventBinding });
+    }
+
+    private void OnDisable()
+    {
+        // TODO: check if we need to unsubscribe. I think it's unnecessary since the UTIl class handles cleanup, and doing it here
+        //       also causes some Collection Modified error.
+        //Type eventType = globalEventType.GetType();
+        //if (eventBinding != null && globalEventType != null)
+        //{
+        //    Type busType = typeof(EventBus<>).MakeGenericType(eventType);
+        //    var unregisterMethod = busType.GetMethod("Unregister");
+        //    unregisterMethod.Invoke(null, new object[] { eventBinding });
+        //}
+    }
+
+    private void OnDestroy()
+    {
+        Type eventType = globalEventType.GetType();
+        if (eventBinding != null && globalEventType != null)
+        {
+            Type busType = typeof(EventBus<>).MakeGenericType(eventType);
+            var unregisterMethod = busType.GetMethod("Unregister");
+            unregisterMethod.Invoke(null, new object[] { eventBinding });
+        }
+    }
+
 
     public void Trigger()
     {
-        e?.Invoke();
+        OnEventTriggered?.Invoke();
     }
+
+    
 }
