b65f6ae6efd80e570014c50da998c2aadbb6c43f
AllenZoo <44107623+AllenZoo@users.noreply.github.com>
2025-10-06 00:46:41 -0700

    [UB-161] added option to disable upgrade pack after usage (#163)

--- Assets/Scripts/Systems/Upgrade/Card/Components/UpgradeCardPack.cs
+++ Assets/Scripts/Systems/Upgrade/Card/Components/UpgradeCardPack.cs
@@ -8,8 +8,28 @@ using UnityEngine;
 /// </summary>
 public class UpgradeCardPack : CardPackBase<UpgradeCardData, OnDisplayUpgradeCardsRequest>
 {
+    [Tooltip("Whether we disable the pack after player selects an option.")]
+    [SerializeField] private bool disableAfterUse = true;
+
+    protected void Start()
+    {
+        if (disableAfterUse)
+        {
+            EventBinding<OnUpgradeCardApplyEffect> onUpgradeCardApplyEffectBinding = new EventBinding<OnUpgradeCardApplyEffect>(HandleApplyUpgradeCard);
+            EventBus<OnUpgradeCardApplyEffect>.Register(onUpgradeCardApplyEffectBinding);
+        }
+    }
+
     protected override OnDisplayUpgradeCardsRequest CreateDisplayEvent(HashSet<UpgradeCardData> cards)
     {
-        return new OnDisplayUpgradeCardsRequest { upgradeCards = cards };
+        return new OnDisplayUpgradeCardsRequest { upgradeCards = cards, src = this };
+    }
+
+    private void HandleApplyUpgradeCard(OnUpgradeCardApplyEffect e)
+    {
+        if (disableAfterUse)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
