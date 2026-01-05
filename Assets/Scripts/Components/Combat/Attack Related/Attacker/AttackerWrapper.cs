481e28ee454c22474a4f785576d0a5f7a6800a1f
AllenZoo <allenzhu2000@hotmail.com>
2026-01-05 12:20:00 -0800

    [UB-175] gave chillcrusher upgrade cards!

--- Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerWrapper.cs
+++ Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerWrapper.cs
@@ -1,18 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// Class that wraps multiple different kinds of IAttackers so that they can all 'attack' at once.
 /// 
-/// Note: CD is overwritten by this class.
+/// Note: CD is overwritten by this class. (??, maybe not)
+/// 
+/// TODO: implement for next boss.
 /// </summary>
 public class AttackerWrapper : IAttacker
 {
+    public List<IAttacker> attackers;
+
     public AttackerData AttackerData { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
     public AttackData AttackData { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
     public void Attack(KeyCode keyCode, AttackContext attackContext)
     {
-        throw new System.NotImplementedException();
+        foreach (var attacker in attackers)
+        {
+            attacker.Attack(keyCode, attackContext);
+        }
     }
 
     public bool CanAttack()
