05f821a60be2f5317b33e9ca03831a11d7460dd7
AllenZoo <allenzhu2000@hotmail.com>
2026-01-06 11:21:22 -0800

    [UB-175] worked on AttackWrapper. commit before maj. refactor
    - TODO: chillcrusher rage behaviour

--- Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerComponent.cs
+++ Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerComponent.cs
@@ -14,9 +14,6 @@ public class AttackerComponent : SerializedMonoBehaviour
     [Required, OdinSerialize]
     private IAttacker attacker;
 
-    private IAttacker prevAttacker;
-
-
     [Required, SerializeField] private LocalEventHandler localEventHandler;
 
     [Tooltip("Types of entities this attacker can damage.")]
--- Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerWrapper.cs
+++ Assets/Scripts/Components/Combat/Attack Related/Attacker/AttackerWrapper.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 /// 
 /// Note: CD is overwritten by this class. (??, maybe not)
 /// 
-/// TODO: implement for next boss.
+/// TODO: implement for chillcrusher rage phase.
 /// </summary>
 public class AttackerWrapper : IAttacker
 {
     public List<IAttacker> attackers;
 
+    // TODO: we shoudln't really use this properties.. maybe just leave it here as is for now, we can refactor later :) (+1000%)
     public AttackerData AttackerData { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
     public AttackData AttackData { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
@@ -20,13 +21,27 @@ public class AttackerWrapper : IAttacker
     {
         foreach (var attacker in attackers)
         {
-            attacker.Attack(keyCode, attackContext);
+            if (CheckAttackerStatus(attacker))
+            {
+                // TODO: figure out how to track each indiviual cooldown.
+                attacker.Attack(keyCode, attackContext);
+            }
         }
     }
 
-    public bool CanAttack()
+    public void StopAttack()
     {
         throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Always returns true, CanAttack of each individual attacker will be handled in the main Attack() function.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="System.NotImplementedException"></exception>
+    public bool CanAttack()
+    {
+        return true;
     }
 
     public IAttacker DeepClone()
@@ -34,23 +49,44 @@ public class AttackerWrapper : IAttacker
         throw new System.NotImplementedException();
     }
 
+    /// <summary>
+    /// Similar to CanAttack, individual GetChargeUp willl be handled in main Attack() function.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="System.NotImplementedException"></exception>
     public float GetChargeUp()
     {
+        return 0f;
         throw new System.NotImplementedException();
     }
 
+    /// <summary>
+    /// Similar to CanAttack, individual Cooldowns willl be handled in main Attack() function.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="System.NotImplementedException"></exception>
     public float GetCooldown()
     {
+        return 0f;
         throw new System.NotImplementedException();
     }
 
+    /// <summary>
+    /// Considered initialized as long as we have attackers wrapped.
+    /// </summary>
+    /// <returns></returns>
     public bool IsInitialized()
     {
-        throw new System.NotImplementedException();
+        return attackers.Count > 0;
     }
 
-    public void StopAttack()
+    /// <summary>
+    /// Checks if attacker is ready to attack.
+    /// </summary>
+    /// <param name="attacker"></param>
+    /// <returns></returns>
+    private bool CheckAttackerStatus(IAttacker attacker)
     {
-        throw new System.NotImplementedException();
+        return false; //stub
     }
 }
