9e74889d97f333220b2abf4f36b011e365534c06
AllenZoo <allenzhu2000@hotmail.com>
2026-01-18 22:26:55 -0800

    [UB-126] fixed and re-enabled ring attack

--- Assets/Scripts/Components/Combat/Attack Related/Attack/RingAttack.cs
+++ Assets/Scripts/Components/Combat/Attack Related/Attack/RingAttack.cs
@@ -1,21 +1,24 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-// TODO: refactor this an attack script to use scriptable objects as well as make attackers also able to spawn RingAttacks.
+// TODO-OPT: refactor this an attack script to use scriptable objects as well as make attackers also able to spawn RingAttacks.
 //       Remove this class once we implement a persistent attack behaviour logic, or smt similar. 
 //       This attack can then be digested into a cage attack that is persistent.
-
-// TODO: re-enable (temporarily disabled)
+// Note: temporarily, this implementation is good, don't change unless necessary.
 /// A script that will spawn a ring of attacks around given parameters. The attacks will then spin around the center point.
 public class RingAttack : MonoBehaviour
 {
-    [SerializeField] private List<EntityType> targetTypes = new List<EntityType>();
+    [Required, SerializeField] private AttackerComponent attackerComp;
     [SerializeField] private GameObject attackObj;
     [SerializeField] private float numAttacks = 8;
     [SerializeField] private float radius = 1f;
+
     public float Radius { get { return radius; } set { radius = value; } }
+
+    private List<AttackComponent> spawnedAttacks = new List<AttackComponent>();
 
     private void Awake()
     {
@@ -25,6 +28,7 @@ public class RingAttack : MonoBehaviour
         // Check if attackObj has an Attack component.
         Assert.IsNotNull(attackObj.GetComponent<AttackComponent>(), "RingAttack needs an attack object with an Attack component.");
     }
+
 
     /// <summary>
     /// Spawns the attacks around the given location.
@@ -38,17 +42,45 @@ public class RingAttack : MonoBehaviour
             DeSpawn();
         }
 
-        float angleIncrement = 360f / numAttacks;
-
-        for (int i = 0; i < numAttacks; i++)
+        AttackData attackData = attackObj.GetComponent<AttackComponent>().Attack.AttackData;
+        AttackContext ac = new AttackContext
         {
-            // TODO: temp disabled.
-            // Spawn the attack in a circle around the location point.
-            //Vector2 offsetLoc = new Vector2(location.position.x + radius / 2, location.position.y);
-            //GameObject attack = Instantiate(attackObj, offsetLoc, Quaternion.identity, this.transform);
+            AttackerComponent = attackerComp,
+        };
 
-            ////attack.GetComponent<AttackComponent>().Initialize();
-            //attack.transform.RotateAround(location.position, Vector3.forward, angleIncrement * i);
+        // Spawn attacks WITHOUT movement
+        var spawned = AttackSpawner.SpawnGroup(
+            Mathf.RoundToInt(numAttacks),
+            i =>
+            {
+                float angle = i * Mathf.PI * 2f / numAttacks;
+
+                // Movement handles positioning relative to attacker
+                return NullMovement.Instance;
+            },
+            attackData,
+            ac,
+            new AttackModificationContext(),
+            attackData.AttackPfb.GetComponent<AttackComponent>().Attack,
+            rotationFactory: i =>
+                Quaternion.Euler(0f, 0f, i * (360f / numAttacks))
+        );
+
+        spawnedAttacks = spawned;
+
+        for (int i = 0; i < spawned.Count; i++)
+        {
+            var atk = spawned[i];
+
+            // Parent under cage root
+            atk.transform.SetParent(this.transform, false);
+
+            // Initial placement on outer radius
+            float angle = i * Mathf.PI * 2f / spawned.Count;
+            Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+            atk.transform.localPosition = dir * radius;
+
         }
     }
 
@@ -56,9 +88,9 @@ public class RingAttack : MonoBehaviour
     // TODO: refactor this so that we can reuse the attacks instead of destroying them.
     public void DeSpawn()
     {
-        foreach (Transform child in this.transform)
+        foreach (var attack in spawnedAttacks)
         {
-            Destroy(child.gameObject);
+            Destroy(attack.gameObject);
         }
     }
 
@@ -77,17 +109,4 @@ public class RingAttack : MonoBehaviour
         // For testing purposes
         Spawn(this.transform);
     }
-
-    // Bomb attack.
-    //public void Spawn(Transform location)
-    //{
-    //    float angleIncrement = 360f / numAttacks;
-
-    //    for (int i = 0; i < numAttacks; i++)
-    //    {
-    //        GameObject attack = Instantiate(attackObj, location.position, Quaternion.identity);
-    //        attack.transform.RotateAround(location.position, Vector3.forward, angleIncrement * i);
-    //        attack.GetComponent<Rigidbody2D>().velocity = attack.transform.up * spinSpeed;
-    //    }
-    //}
 }
