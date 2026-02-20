f0c211de03150f2f091ff3dd4893925764d9252b
AllenZoo <allenzhu2000@hotmail.com>
2026-02-19 18:28:09 -0800

    [UB-211] added new OnLoadGameRequest event

---
+++ Assets/Scripts/Systems/DataPersistence/SceneDataPersistenceHandler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SceneDataPersistenceHandler : MonoBehaviour, IDataPersistence
+{
+    public void LoadData(GameData data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SaveData(GameData data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
